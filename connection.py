436fe202c0f628314a751c0868dcdef3f9da85dd
florineless@gmail.com <florineless@gmail.com>
2020-01-17 12:14:22 +0200

    connection functions added + few template design

---
+++ connection.py
@@ -0,0 +1,24 @@
+import csv
+
+# This function takes a csv as argument
+# It returns a dict
+
+def read_from_csv(file):
+    with open(file, newline=" ") as csv_data:
+        reader = csv.reader(csv_data)
+    return reader
+
+# This function takes a csv and a list as arguments:(file = csv file, fieldnames = list)
+# Doesn't return anything
+
+def write_to_csv(file, fieldnames):
+    with open(file, "w", newline=" ") as csv_data:
+        writer = csv.writer(csv_data, fieldnames=fieldnames)
+
+# This function appends to a csv file
+# It takes a csv and a list as arguments:(file = csv file, fieldnames = list)
+# Doesn't return anything
+
+def append_to_csv(file, fieldnames):
+    with open(file, "a", newline=" ") as csv_data:
+        writer = csv.writer(csv_data, fieldnames=fieldnames)
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask , request, redirect, render_template, url_for
+import connection, data_manager, utility
 
 app = Flask(__name__, template_folder="template", static_folder="static")
 
@@ -7,7 +8,5 @@ def index_route():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(
-        Debug=True,
-        Port=5000
-    )
+    app.run(debug=True,
+            port=5000)
