b5abc61455e42b821b7ce1bfb9f4909fb5767b87
Florin C <florinciocirlan@Andreeas-MacBook-Pro.local>
2020-02-06 19:57:03 +0200

    delete question , edit question done

--- server.py
+++ server.py
@@ -32,7 +32,6 @@ def list_route():
 def question_route(id):
     question = data_manager.get_question(id)
     answers = data_manager.get_all_answers(id)
-    print(answers)
     return render_template("question-page.html", to_display=question, answers_to_display=answers, question_id=id)
 
 
@@ -56,6 +55,27 @@ def answer_route(question_id):
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/question/<question_id>/edit", methods=["GET", "POST"])
+def edit_route(question_id):
+    if request.method == "GET":
+        question = data_manager.get_question(question_id)
+        return render_template("edit-question.html", question=question, question_id=question_id)
+    elif request.method == "POST":
+        title=request.form['title']
+        message=request.form['message']
+        data_manager.update_data("question",title,message,question_id)
+        return redirect(url_for('question_route', id=question_id))
+
+@app.route("/question/<question_id>/delete", methods=["GET","POST"])
+def delete_question(question_id):
+    if request.method == "GET":
+        return render_template("delete-question.html",question_id=question_id)
+    elif request.method == "POST":
+        data_manager.delete_row("answer", "question_id", question_id)
+        data_manager.delete_row("question", "id", question_id)
+        return redirect(url_for("list_route"))
+
 
 
 if __name__ == "__main__":
